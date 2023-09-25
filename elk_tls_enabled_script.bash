5834f51b402cffb6c97367be67aebbf35664bf02
laba94 <146012032+laba94@users.noreply.github.com>
2023-09-25 19:47:35 +0300

    Update elk_tls_enabled_script.bash

--- elk_tls_enabled_script.bash
+++ elk_tls_enabled_script.bash
@@ -2,7 +2,7 @@ git clone https://github.com/deviantony/docker-elk.git
 cd docker-elk
 
 git checkout tls
-docker-compose up tls
+sudo docker-compose up tls
 
 sudo docker-compose up setup
 sudo docker-compose up
