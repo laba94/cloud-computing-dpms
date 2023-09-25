32791e41b7ac2cfd52b054d8ef99628ae7901fd1
laba94 <146012032+laba94@users.noreply.github.com>
2023-09-25 19:27:09 +0300

    Update filebeat_configuration_script.sh

--- filebeat_configuration_script.sh
+++ filebeat_configuration_script.sh
@@ -1,4 +1,4 @@
 # Install filebeat
-docker pull docker.elastic.co/beats/filebeat:8.10.2
+sudo docker pull docker.elastic.co/beats/filebeat:8.10.2
 # Run filebeat
 sudo docker run docker.elastic.co/beats/filebeat:8.10.2   -E setup.kibana.host=192.168.1.22:5601   -E output.elasticsearch.hosts=["192.168.1.22:9200"]   -E output.elasticsearch.username=elastic   -E output.elasticsearch.password=changeme   -E filebeat.inputs='[{"paths":["/var/log/*"],"type":"log"}]'
